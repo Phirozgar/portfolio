b25a49acc0e541339301c9cad0913371f6deac51
Phirozgar Irani <phirozgarirani11@gmail.com>
2025-11-02 14:12:44 +0530

    Created and Added Blog page

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import FloatingNav from "../components/FloatingNav.jsx";
 import HeroSection from "../components/HeroSection";
-import Carousel3D from "../components/Carousel3D";
-import StoryText from "../components/StoryText";
+import ManipalSection from "../components/ManipalSection.jsx";
 import EducationBento from "../components/EducationBento";
+import SectionDivider from "../components/SectionDivider.jsx";
 
 const Blog = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#0f0c29] via-[#302b63] to-[#24243e] text-white overflow-x-hidden">
       <FloatingNav />
       <HeroSection />
-      <section className="relative flex flex-col items-center justify-center min-h-[100vh] mt-[-10vh]">
-        <Carousel3D />
-        <StoryText />
-      </section>
+      <SectionDivider from="#000000" to="#1b1035" />
       <EducationBento />
+      <SectionDivider from="#1b1035" to="#000000" />
+      <ManipalSection />
     </div>
   );
 };
